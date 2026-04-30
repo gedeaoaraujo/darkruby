82705ac0984ab662feb89870915de14039a55098
gedeoaraujo <gedeao.lemobs@gmail.com>
2026-04-30 15:05:39 -0300

    Adding provider scope.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:darkruby/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const App());
+  runApp(ProviderScope(child: const App()));
 }
 
 const String _appName = 'Dark Ruby';
