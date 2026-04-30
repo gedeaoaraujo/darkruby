7a15ffe54004796892b382ef0a2730d712509582
gedeoaraujo <gedeao.lemobs@gmail.com>
2026-04-30 15:06:30 -0300

    Constructing notes list.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,39 +1,45 @@
 import 'package:darkruby/list_item.dart';
+import 'package:darkruby/notes_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
-class HomePage extends StatefulWidget {
+final notesViewModel = 
+  StateNotifierProvider<NotesViewmodel, NoteState>(
+    (ref) => NotesViewmodel()
+);
+
+class HomePage extends ConsumerWidget {
   final String title;
 
   const HomePage({super.key, required this.title});
-
+  
   @override
-  State<HomePage> createState() => _HomePageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = Theme.of(context);
+    final state = ref.watch(notesViewModel);
 
-class _HomePageState extends State<HomePage> {
-
-  String genStrs(){
-    final chars = List.generate(255, (_) => 'x');
-    return chars.join();
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: theme.colorScheme.inversePrimary,
+        title: Text(title),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            ListItem(text: genStrs()),
-            ListItem(text: genStrs()),
-            ListItem(text: genStrs()),
-          ],
+        child: Builder(
+          builder: (_) => ListView.builder(
+            itemCount: state.notes.length,
+            itemBuilder: (_, i){
+              final item = state.notes[i];
+              return ListItem(
+                title: item.title,
+                date: item.date,
+                text: item.text,
+              );
+            }
+          ) 
         ),
-      )
+      ),
     );
   }
 }
