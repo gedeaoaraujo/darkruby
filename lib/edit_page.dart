38d5fbd6c87b5b70b242c570bbfe8342928303fe
gedeoaraujo <gedeao.lemobs@gmail.com>
2026-05-01 17:47:54 -0300

    Using readonly in viewmode.

--- lib/edit_page.dart
+++ lib/edit_page.dart
@@ -4,14 +4,29 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class EditPage extends ConsumerWidget {
   final Note note;
-  final bool editable;
+  final bool viewMode;
   final _pageTitle = 'Edit Page';
   
   const EditPage({
-    super.key, 
-    this.editable = false, 
+    super.key,
+    this.viewMode = true, 
     this.note = const Note()
   });
+
+  TextEditingController get titleController => .fromValue(.new(
+    text: note.title,
+    selection: .collapsed(offset: note.title.length),
+  ));
+
+  TextEditingController get dateController => .fromValue(.new(
+    text: note.date,
+    selection: .collapsed(offset: note.date.length),
+  ));
+
+  TextEditingController get textController => .fromValue(.new(
+    text: note.text,
+    selection: .collapsed(offset: note.text.length),
+  ));
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -21,45 +36,38 @@ class EditPage extends ConsumerWidget {
       backgroundColor: theme.colorScheme.inversePrimary,
       appBar: AppBar(title: Text(_pageTitle)),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              enabled: editable,
-              controller: .fromValue(TextEditingValue(
-                text: note.title,
-                selection: TextSelection.collapsed(offset: note.title.length),
-              )),
-              decoration: InputDecoration(
-              border: .none,
-              labelText: 'Título',
-            )),
-            TextField(
-              enabled: false,
-              controller: .fromValue(TextEditingValue(
-                text: note.date,
-                selection: TextSelection.collapsed(offset: note.date.length),
-              )),
-              decoration: InputDecoration(
-                border: .none,
-                labelText: 'Data',
-              )
-            ),
-            Expanded(
-              child: TextField(
-                maxLines: null,
-                enabled: editable,
-                controller: .fromValue(TextEditingValue(
-                  text: note.text,
-                  selection: TextSelection.collapsed(offset: note.text.length),
-                )),
-                decoration: InputDecoration(
+        padding: const .all(16.0),
+        child: Expanded(
+          child: Column(
+            children: [
+              TextField(
+                readOnly: viewMode,
+                controller: titleController,
+                decoration: .new(
                   border: .none,
-                  labelText: 'Texto',
+                  labelText: 'Título',
+                )),
+              TextField(
+                readOnly: viewMode,
+                controller: dateController,
+                decoration: .new(
+                  border: .none,
+                  labelText: 'Data',
                 )
               ),
-            ),
-          ],
+              Expanded(
+                child: TextField(
+                  maxLines: null,
+                  readOnly: viewMode,
+                  controller: textController,
+                  decoration: .new(
+                    border: .none,
+                    labelText: 'Texto',
+                  )
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
