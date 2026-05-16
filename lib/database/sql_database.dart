2fa8d8c7eec2cb7615a3b1e50a2c4057c826f55b
gedeoaraujo <gedeao.lemobs@gmail.com>
2026-05-16 16:55:09 -0300

    Changing get path directory.

--- lib/database/sql_database.dart
+++ lib/database/sql_database.dart
@@ -1,17 +1,22 @@
 import 'dart:async';
-import 'package:sqflite/sqflite.dart';
+import 'dart:io' as io;
 import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 class SqlDatabase {
   
   static late final Database database;
 
   static FutureOr<void> init() async {
-    final databasesPath = await getDatabasesPath();
-    final path = join(databasesPath, 'darkruby.db');
-    database = await openDatabase(
-      path, version: 1,
-      onCreate: (Database db, int version) async {
+    sqfliteFfiInit();
+    final io.Directory dir = await getApplicationDocumentsDirectory();
+    final path = join(dir.path, 'databases', 'darkruby.db');
+
+    final dbFactory = databaseFactoryFfi;
+    database = await dbFactory.openDatabase(
+      path, options: .new(version: 1, onCreate:
+      (Database db, int version) async {
         String query = 'create table if not exists Notes(';
         query += 'id integer primary key autoincrement,';
         query += 'title text not null,';
@@ -19,12 +24,12 @@ class SqlDatabase {
         query += 'text text not null)';
         await db.execute(query);
       }
-    );
+    ));
   }
 
   static Future<void> delete() async {
-    final databasesPath = await getDatabasesPath();
-    final path = join(databasesPath, 'darkruby.db');
+    final io.Directory dir = await getApplicationDocumentsDirectory();
+    final path = join(dir.path, 'databases', 'darkruby.db');
     deleteDatabase(path);
   }
 
