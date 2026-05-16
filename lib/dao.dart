fff2ef4808520cd1acc7660373f5ee4ba34041e1
gedeoaraujo <gedeao.lemobs@gmail.com>
2026-05-16 13:37:28 -0300

    Creating an abstract dao class.

---
+++ lib/dao.dart
@@ -0,0 +1,54 @@
+import 'package:darkruby/sql_database.dart';
+import 'package:darkruby/types.dart';
+
+abstract class Dao {
+
+  abstract final String tableName;
+
+  Future<void> insert(dynamic entity) async {
+    final db = SqlDatabase.database;
+    await db.insert(
+      tableName,
+      entity.toMap(),
+      conflictAlgorithm: .replace
+    );
+  }
+
+  Future<AnyMap?> getById(dynamic entity) async {
+    final db = SqlDatabase.database;
+    final obj = entity.toMap();
+    final maps = await db.query(
+      tableName,
+      where: 'id = ?',
+      whereArgs: [obj.id], 
+      columns: entity.keys.toList(),
+    );
+    return maps.firstOrNull;
+  }
+
+  Future<List<AnyMap>> getAll() async {
+    final db = SqlDatabase.database;
+    final result = await db.query(tableName);
+    return result;
+  }
+
+  Future<void> deleteById(int id) async {
+    final db = SqlDatabase.database;
+    await db.delete(
+      tableName, 
+      where: 'id = ?',
+      whereArgs: [id]
+    );
+  }
+
+  Future<void> update(dynamic entity) async {
+    final db = SqlDatabase.database;
+    await db.update(
+      tableName,
+      entity.toMap(),
+      where: 'id = ?',
+      whereArgs: [entity.id]
+    );
+  }
+
+}
