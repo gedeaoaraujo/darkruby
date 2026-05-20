38a3419c2555bfa40593c2637ede61d18612745f
gedeoaraujo <gedeao.lemobs@gmail.com>
2026-05-20 17:02:21 -0300

    Managing password checking.

--- lib/widgets/password_dialog.dart
+++ lib/widgets/password_dialog.dart
@@ -1,24 +1,36 @@
 import 'package:flutter/material.dart';
 
-class PasswordDialog extends StatelessWidget {
-  const PasswordDialog({
-    super.key,
-    required TextEditingController password,
-    required TextEditingController confirm,
-  }) : _password = password, _confirm = confirm;
+class PasswordDialog extends StatefulWidget {
+  const PasswordDialog({super.key});
 
-  final TextEditingController _password;
-  final TextEditingController _confirm;
+  @override
+  State<PasswordDialog> createState() => _PasswordDialogState();
+}
+
+class _PasswordDialogState extends State<PasswordDialog> {
+  bool showError = false;
+  final TextEditingController _confirm = .new();
+  final TextEditingController _password = .new();
+
+  void onPressOK() {
+    setState(() {
+      showError = _password.text != _confirm.text;
+    });
+    
+    if (showError) return;
+    Navigator.pop(context);
+  }
 
   @override
   Widget build(BuildContext context) {
+    final scheme = Theme.of(context).colorScheme;
     return AlertDialog(
       title: Padding(
         padding: const EdgeInsets.all(8.0),
         child: const Text('Modify Password'),
       ),
       content: SizedBox(
-        height: 150,
+        height: 180,
         child: Column(
           spacing: 32,
           children: [
@@ -36,12 +48,20 @@ class PasswordDialog extends StatelessWidget {
                 floatingLabelBehavior: .always,
               ),
             ),
+            if (showError) Text(
+              'Passwords is not equals',
+              style: .new(color: scheme.error)
+            )
           ]
         ),
       ),
       actions: <Widget>[
         TextButton(
           onPressed: () => Navigator.pop(context),
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: onPressOK,
           child: const Text('OK'),
         ),
       ],
