2e8ccd5f7718e3b866592452670429c9d9487b96
gedeoaraujo <gedeao.lemobs@gmail.com>
2026-05-20 21:50:07 -0300

    Setting more configs for biometrics.

--- android/app/src/main/kotlin/com/example/darkruby/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/darkruby/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.example.darkruby
 
-import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle;
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState);
+  }
+}
+
+// import io.flutter.embedding.android.FlutterActivity
+// class MainActivity : FlutterActivity()
