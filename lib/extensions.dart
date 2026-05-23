6a3a3cbc800fcbd05c509b3dd0df8c2c856b3841
gedeoaraujo <gedeao.lemobs@gmail.com>
2026-05-23 00:13:34 -0300

    Creating date formater.

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,3 +1,13 @@
+import 'package:intl/intl.dart';
+
 extension BoolExtensions on bool {
   bool not() => !this;
+}
+
+extension StrExtensions on String {
+  String toPtBrDateTime() {
+    final fmt = DateFormat('dd/MM/yyyy kk:mm');
+    final date = DateTime.parse(this);
+    return fmt.format(date);
+  }
 }
