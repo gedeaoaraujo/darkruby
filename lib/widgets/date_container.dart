7372e6c6be7af6e6fad008e95d79b1f044ccbb00
gedeoaraujo <gedeao.lemobs@gmail.com>
2026-05-25 20:59:57 -0300

    Adding icons.

--- lib/widgets/date_container.dart
+++ lib/widgets/date_container.dart
@@ -10,50 +10,66 @@ class DateContainer extends StatelessWidget {
     final textTheme = Theme.of(context).textTheme;
     final width = MediaQuery.sizeOf(context).width;
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 8),
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 0),
       child: Row(
+        spacing: 0,
         children: [
           SizedBox(
-            width: width * 0.70,
+            width: width * 0.5,
             child: ElevatedButton(
               onPressed: (){},
               style: ElevatedButton.styleFrom(
+                elevation: 0,
                 alignment: .centerStart,
                 padding: EdgeInsetsGeometry.symmetric(
-                  horizontal: 0 ,vertical: 8
+                  horizontal: 4 ,vertical: 8
                 ),
                 shape: RoundedRectangleBorder(borderRadius: .circular(8))
               ),
-              child: Column(
-                crossAxisAlignment: .start,
+              child: Row(
+                spacing: 16,
                 children: [
-                  Text(date.toDay, style: .new(
-                    fontSize: textTheme.titleMedium?.fontSize
-                  )),
-                  Text(date.toYear, style: .new(
-                    fontSize: textTheme.titleMedium?.fontSize
-                  ))
+                  Icon(Icons.calendar_today),
+                  Column(
+                    crossAxisAlignment: .start,
+                    children: [
+                      Text(date.toDay, style: .new(
+                        fontSize: textTheme.titleMedium?.fontSize
+                      )),
+                      Text(date.toYear, style: .new(
+                        fontSize: textTheme.titleMedium?.fontSize
+                      ))
+                    ],
+                  ),
                 ],
               ),
             ),
           ),
+          Spacer(),
           SizedBox(
-            width: width * 0.20,
+            width: width * 0.25,
             child: ElevatedButton(
               onPressed: (){},
               style: ElevatedButton.styleFrom(
-                alignment: .centerEnd,
-                padding: EdgeInsets.symmetric(vertical: 8),
+                elevation: 0,
+                alignment: .center,
+                padding: EdgeInsets.symmetric(horizontal: 4, vertical: 8),
                 shape: RoundedRectangleBorder(borderRadius: .circular(8))
               ),
               child: Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Text(
-                  date.toHour,
-                  textAlign: .end, 
-                  style: .new(
-                    fontSize: textTheme.titleMedium?.fontSize
-                  )
+                child: Row(
+                  spacing: 16,
+                  children: [
+                    Icon(Icons.av_timer),
+                    Text(
+                      date.toHour,
+                      textAlign: .center, 
+                      style: .new(
+                        fontSize: textTheme.titleMedium?.fontSize
+                      )
+                    ),
+                  ],
                 ),
               ),
             ),
