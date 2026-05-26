2abefd203d6f50c3eb0804c144019b8323617802
gedeoaraujo <gedeao.lemobs@gmail.com>
2026-05-25 22:42:49 -0300

    Adding new date extensions.

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -28,4 +28,24 @@ extension StrExtensions on String {
     final fmt = DateFormat('yyyy, EEEE');
     return fmt.format(dt);
   }
+
+  DateTime toDateTime() => DateTime.parse(this);
+}
+
+extension DateExtensions on DateTime {
+  DateTime copyWith({
+    int? year,
+    int? month,
+    int? day,
+    int? hour,
+    int? minute,
+    int? second,
+  })=> DateTime(
+      year ?? this.year,
+      month ?? this.month,
+      day ?? this.day,
+      hour ?? this.hour,
+      minute ?? this.minute,
+      second ?? this.second,
+    );
 }
